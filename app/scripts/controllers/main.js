1aad1738b1373d985e613861e7c17ddb4f20ede2
cawdev <cameronweimer@gmail.com>
2016-11-29 18:08:10 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name crosschxCodeTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the crosschxCodeTestApp
+ */
+angular.module('crosschxCodeTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
